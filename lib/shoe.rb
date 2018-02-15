0a528f2af1532bb0c111c58aaec0c869fde468e7
Neil Seejoor <nvseejoor@gmail.com>
2018-02-15 14:37:04 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  BRANDS.uniq = []
   def initialize(brand)
     @brand = brand
     BRANDS << brand
-    
+
   end
 
 
