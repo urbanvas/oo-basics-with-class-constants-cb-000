45c06626b1ce8ea3fc2bd90e7eba0b4bcbc5f223
Neil Seejoor <nvseejoor@gmail.com>
2018-02-15 14:38:47 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,10 +3,11 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
+  array []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-
+    array << brand
+    BRANDS << array.uniq
   end
 
 
